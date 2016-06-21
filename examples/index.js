5dec52d9a417626d8ca91ef2f7d94f1b137b98a1
Lorenzo Mussi <lorenzo.mussi@buongiorno.com>
2016-06-21 11:28:13 +0200

    examples complete

--- examples/index.js
+++ examples/index.js
@@ -1,3 +1,62 @@
 var logger = new LoggerPro.BaseLogger();
+var rotLogger = new LoggerPro.RotatingLogger();
 
-logger.log('cuigbbzdfkl');
+logger.init({
+	level: 'log', //set all level to true
+    enabled: true //enables logger
+});
+
+rotLogger.init({
+	level: 'log', //set all level to true
+    enabled: true //enables logger
+})
+
+var log = function(){
+	logger.log('pio');
+	rotLogger.log('pio');
+}
+
+var info = function(){
+	logger.info('pio');
+	rotLogger.info('pio');
+}
+
+var table = function(){
+	logger.table('pio');
+	rotLogger.table('pio');
+}
+
+var warn = function(){
+	logger.warn('pio');
+	rotLogger.warn('pio');
+}
+
+var error = function(){
+	logger.error('pio');
+	rotLogger.error('pio');
+}
+
+var enable = function(){
+	logger.init({
+		enabled: true
+	});
+
+	rotLogger.init({
+		enabled:true
+	});
+}
+var disable = function(){
+	logger.init({
+		enabled: false
+	});
+
+	rotLogger.init({
+		enabled:false
+	});
+}
+var startRecording = function(){
+	rotLogger.startRecording();
+}
+var endRecording = function(){
+	rotLogger.endRecording();
+}
