7fd364634085d0eac3fbbf90afd0d48963798064
aureliome <aurelio.me@gmail.com>
2016-07-12 16:28:11 +0200

    Version 2.0.1

---
+++ 2.0.1/js/docs-setup.js
@@ -0,0 +1,43 @@
+NG_DOCS={
+  "sections": {
+    "version": "API Reference"
+  },
+  "pages": [
+    {
+      "section": "version",
+      "id": "logger-pro",
+      "shortName": "logger-pro",
+      "type": "overview",
+      "moduleName": "logger-pro",
+      "shortDescription": "Logger Pro",
+      "keywords": "advanced call components disabled enable enabled init levels log logger logger-pro module ordered organized overview pro relevance relevant remember rotatinglogger sorted system version"
+    },
+    {
+      "section": "version",
+      "id": "logger-pro.Logger",
+      "shortName": "logger-pro.Logger",
+      "type": "object",
+      "moduleName": "logger-pro",
+      "shortDescription": "Provides you the basic functionality of a log system.",
+      "keywords": "actives addictional allows args arguments attributes basic call change config configuration console custom defines disabled emit emitter enable enabled error errors example examples false file function functionality general getconfig getter hello iclude include included info init initialization initialize isenabled javascript js level levels log logger logger-pro logs manual message messages method module object options parameter personalized provided read-only script sends src store system table true version warn warning warnings window world"
+    },
+    {
+      "section": "version",
+      "id": "logger-pro.RotatingLogger",
+      "shortName": "logger-pro.RotatingLogger",
+      "type": "object",
+      "moduleName": "logger-pro",
+      "shortDescription": "Provides you an advanced functionality of a log system.",
+      "keywords": "adding advanced api api_send arguments attribute attributes automatically call calling calls change click configuration configure custom defines disabled dist dist-rotating documentation emit enabled enables endrecording error example export file files find folder function functionality general getconfig hello include included info init initialization initialize internal javascript js json length level list log logged logger logger-pro max maxsize message messages method methods_getconfig methods_init mode module number object options original provided record recorded recording recordingenabled records rotating rotatinglogger save saverecords saves sends set sliding src start startrecording starts system table tells true version warn window world wraps"
+    }
+  ],
+  "apis": {
+    "version": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/version/logger-pro",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
